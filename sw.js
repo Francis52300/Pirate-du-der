c22a5291c5e43b65faf0821d8cbccd65efd22e78
Francis52300 <f.edot@free.fr>
2026-04-22 18:57:49 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -6,9 +6,9 @@
    - APIs externes (fonts, Leaflet, React) : Stale While Revalidate
    ================================================================ */
 
-const CACHE_VERSION    = 'pirate-quest-v3';
-const TILE_CACHE       = 'pirate-tiles-v3';
-const ASSETS_CACHE     = 'pirate-assets-v3';
+const CACHE_VERSION    = 'pirate-quest-v4';
+const TILE_CACHE       = 'pirate-tiles-v4';
+const ASSETS_CACHE     = 'pirate-assets-v4';
 
 /* Assets à précacher au moment de l'installation */
 const PRECACHE_ASSETS = [
